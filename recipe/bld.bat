fc86b05cc53c463d4c2724197e6f5e7b5db56114
Silvio Traversaro <silvio.traversaro@iit.it>
2020-11-26 19:18:59 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -16,6 +16,7 @@ cmake ^
     -DTBB_FOUND=1 ^
     -DTBB_INCLUDEDIR=%LIBRARY_PREFIX%\include ^
     -DTBB_LIBRARY_DIR=%LIBRARY_PREFIX%\lib ^
+    -DUSE_EXTERNAL_TINY_PROCESS_LIBRARY=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 

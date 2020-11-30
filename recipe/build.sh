e934ffaa27eb66dbb9f1020c6f688d7c5fb3806c
Tobias Fischer <info@tobiasfischer.info>
2020-11-30 15:06:44 +1000

    Copy activate scripts in build.sh
    See https://github.com/conda-forge/gazebo-feedstock/commit/be8f87d9b9dbc05fede08c3f713f8d856af2f0bb

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,3 @@
-
 mkdir build
 cd build
 
@@ -14,3 +13,11 @@ cmake .. \
 
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done

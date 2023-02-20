14eb5d5b3f7778f359f84db405dd3dc85dbb7912
Tobias Fischer <info@tobiasfischer.info>
2023-02-20 14:23:26 +1000

    Add GAZEBO_PLUGIN_PATH to DYLD_LIBRARY_PATH on OSX
    See https://github.com/RoboStack/ros-noetic/issues/228 and https://github.com/ros-simulation/gazebo_ros_pkgs/pull/974/files

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,1 +1,7 @@
 source "${CONDA_PREFIX}/share/gazebo/setup.sh"
+
+_UNAME=`uname -s`
+if [ "$_UNAME" = "Darwin" ]; then
+  export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${GAZEBO_PLUGIN_PATH}
+fi
+unset _UNAME
